b36c50b808ef155e19ccdb948ed306c3ba5dae8f
Erkin İşçi <erkin.isci@matriksdata.com>
2016-10-13 12:39:06 +0300

    First Page Ekran tasarimi

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -6,48 +6,60 @@ using Matriks.Wpf.Models;
 
 namespace Matriks.ClientAPI.Setup
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+      InitializeComponent();
 
-            DataContext = this;
+      DataContext = this;
 
-            Menus = new ObservableCollection<MenuModel>();
+      Menus = new ObservableCollection<MenuModel>();
 
-            Loaded += MainWindow_Loaded;
+      Loaded += MainWindow_Loaded;
 
-            AddMenu("FirstPage", "views.firstpage");
-        }
+      AddMenu("collecting", "views.firstpage");
+      AddMenu("FirstPage", "views.firstpage");
 
-        protected void AddMenu(string name, string nameSpace)
-        {
-            Menus.Add(new MenuModel { Name = name, ViewNamespace = nameSpace });
-        }
-
-        void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            Navigate("/views/firstpage.xaml");
-        }
-
-        private void Navigate(string viewName)
-        {
-            var n = NavigationService.Navigate(viewName, true);
-            LayoutRoot.Navigate(n.View);
-        }
-
-        public ObservableCollection<MenuModel> Menus
-        {
-            get { return (ObservableCollection<MenuModel>)GetValue(MenusProperty); }
-            set { SetValue(MenusProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Menus.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MenusProperty = DependencyProperty.Register("Menus", typeof(ObservableCollection<MenuModel>), typeof(MainWindow));
-
+      MenuListBox.SelectionChanged += MenuListBox_SelectionChanged;
     }
+
+    private void MenuListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      foreach (var menuModel in Menus)
+        menuModel.IsActive = false;
+
+      Menus[MenuListBox.SelectedIndex].IsActive = true;
+      Navigate(Menus[MenuListBox.SelectedIndex].Root);
+    }
+
+    protected void AddMenu(string name, string nameSpace)
+    {
+      Menus.Add(new MenuModel { Name = name, ViewNamespace = nameSpace });
+    }
+
+    void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      Navigate("/views/firstpage.xaml");
+    }
+
+    private void Navigate(string viewName)
+    {
+      var n = NavigationService.Navigate(viewName, true);
+      LayoutRoot.Navigate(n.View);
+    }
+
+    public ObservableCollection<MenuModel> Menus
+    {
+      get { return (ObservableCollection<MenuModel>)GetValue(MenusProperty); }
+      set { SetValue(MenusProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Menus.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty MenusProperty = DependencyProperty.Register("Menus", typeof(ObservableCollection<MenuModel>), typeof(MainWindow));
+
+  }
 }
