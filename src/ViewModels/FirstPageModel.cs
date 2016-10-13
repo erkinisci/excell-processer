b116a2abedb669709a892aa07a56f575c9fbf7fe
Erkin İşçi <erkin.isci@matriksdata.com>
2016-10-13 14:26:15 +0300

    Dosya konumu secme commandi

--- src/ViewModels/FirstPageModel.cs
+++ src/ViewModels/FirstPageModel.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Forms;
+using Matriks.Oms.EnterpriseLibrary;
+using Matriks.Oms.EnterpriseLibrary.Common;
+using Matriks.Oms.EnterpriseLibrary.Configuration;
 using Matriks.Wpf;
+using Matriks.Wpf.Framework.Commands;
 
 namespace Matriks.ClientAPI.Setup.ViewModels
 {
   public class FirstPageModel : NavigateObject
   {
+    private IAppSettings _appSettings;
+    public DelegateCommand FilePathDialogCommand { get; set; }
+
     public override void OnNavigate(UriQuery query)
     {
       base.OnNavigate(query);
-
-
     }
 
     public override void OnLoaded(FrameworkElement view)
     {
       base.OnLoaded(view);
 
-      FilePath = "C:ProgramFiles";
+      _appSettings = DependencyContainer.Resolver.GetService<IAppSettings>("UISettings");
+
+      FilePath = _appSettings.GetString("FilePath");
+      FilePathDialogCommand = new DelegateCommand(OnFilePathDialogCommand);
+    }
+
+    private void OnFilePathDialogCommand()
+    {
+      using (var dlg = new FolderBrowserDialog())
+      {
+        dlg.SelectedPath = FilePath;
+        var result = dlg.ShowDialog();
+        if (result == DialogResult.OK)
+        {
+          FilePath = dlg.SelectedPath;
+        }
+      }
     }
 
     public string FilePath
