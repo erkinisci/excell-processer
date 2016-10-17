9d9ea42e2c982638e0a5603f98d2c05f6e0c0dcc
Erkin İşçi <erkin.isci@matriksdata.com>
2016-10-17 15:21:32 +0300

    shortcut loglama uslemi

--- src/Models/AppPreferencesModel.cs
+++ src/Models/AppPreferencesModel.cs
@@ -6,32 +6,48 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Matriks.ClientAPI.Setup.Core;
+using Matriks.Oms.EnterpriseLibrary;
+using Matriks.Oms.EnterpriseLibrary.Common;
+
 //using IWshRuntimeLibrary;
 
 namespace Matriks.ClientAPI.Setup.Models
 {
   public class AppPreferencesModel
   {
+    public static ISetupLogger SetupLogger { get; set; }
     public static  void CreateShortcut()
     {
+
+      SetupLogger = DependencyContainer.Resolver.GetService<ISetupLogger>("SetupLogger");
+
       var appInfo = MatriksClientApiSetupModel.FindApp("clientapimanager");
       if (appInfo == null)
         return;
+
       var mts = new MatriksClientApiSetupModel();
 
-      Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8")); //Windows Script Host Shell Object
+      SetupLogger.WriteInfoLog("Windows Script Host Shell Object reflection islemleri basliyor.");
+      var t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8")); //Windows Script Host Shell Object
       dynamic shell = Activator.CreateInstance(t);
       try
       {
         var shDesktop = (object)"Desktop";
-        string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\" + appInfo.ShortCutName + ".lnk";
+        var shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\" + appInfo.ShortCutName + ".lnk";
         var lnk = shell.CreateShortcut(shortcutAddress);
         try
         {
 
           lnk.TargetPath = Path.Combine(mts.MainFolderPath, appInfo.FolderName, appInfo.ExeName);
-          lnk.IconLocation = Path.Combine(lnk.TargetPath+ ", 0");
+          lnk.IconLocation = Path.Combine(lnk.TargetPath + ", 0");
           lnk.Save();
+
+          SetupLogger.WriteErrorLog("Windows Script Host Shell Object reflection islemlerinde hata olustu. Kisa yol hazirlandi.");
+        }
+        catch (Exception ex)
+        {
+          SetupLogger.WriteErrorLog("Windows Script Host Shell Object reflection islemlerinde hata olustu. Kisa yol hazirlanamadi. Hata:" + ex.StackTrace);
         }
         finally
         {
